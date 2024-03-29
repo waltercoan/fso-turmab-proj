013143e2e1d71c49c64f5e08f8627af0afc9f4b7
WALTER SILVESTRE COAN <walter.s@univille.br>
2024-03-28 22:08:32 -0300

    first commit

---
+++ src/test/java/br/univille/projfso2024b/Projfso2024bApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projfso2024b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projfso2024bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
