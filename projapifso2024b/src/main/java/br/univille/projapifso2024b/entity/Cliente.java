4eebef79a03f2f9dd5faf9c92a2c9e63a113f1a1
WALTER SILVESTRE COAN <walter.s@univille.br>
2024-04-25 21:48:26 -0300

    alterar

--- projapifso2024b/src/main/java/br/univille/projapifso2024b/entity/Cliente.java
+++ projapifso2024b/src/main/java/br/univille/projapifso2024b/entity/Cliente.java
@@ -2,6 +2,8 @@ package br.univille.projapifso2024b.entity;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
